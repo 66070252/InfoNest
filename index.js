654591f459eef15ab00b742b1dd2906339291fed
66070252 <66070252@kmitl.ac.th>
2025-08-06 00:53:45 +0700

    submit 2
    Example

--- index.js
+++ index.js
@@ -4,12 +4,24 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
+// view engine เป็น EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
 
 app.get('/', (req, res) => {
-    res.send('สวัสดีโลก');
+  const items = [
+    { name: 'ซื้อของในซูเปอร์' },
+    { name: 'ออกกำลังกาย' },
+    { name: 'อ่านหนังสือ' },
+    { name: 'เขียนโปรเจกต์เว็บ' }
+  ];
+
+  res.render('main', { items });
 });
 
-// เริ่มต้น server
+// server
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
