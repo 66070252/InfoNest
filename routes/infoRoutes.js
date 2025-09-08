6218a9cd7593abce0eaa435e258bdcdea337670c
66070252 <66070252@kmitl.ac.th>
2025-09-09 02:16:45 +0700

    update swagger
    Add 1 api swagger

--- routes/infoRoutes.js
+++ routes/infoRoutes.js
@@ -1,6 +1,29 @@
 import infoController from "../controllers/infoController.js"
 
 const useUserRoute = async (router) => {
+  /**
+ * @swagger
+ * tags:
+ *  name: Info
+ *  description: Info management endpoints
+ */
+
+/**
+ * @swagger
+ * /api/products:
+ *  get:
+ *      summary: Get all info
+ *      tags: [Info]
+ *      responses:
+ *          200:
+ *              description: A list of Info
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: array
+ *                          items:
+ *                              $ref: '#/components/schemas/Info'
+ */
   router.get('/info', infoController.getAllInfo)
   router.get('/info/:id', infoController.getInfoById)
   router.post('/info', infoController.create)
